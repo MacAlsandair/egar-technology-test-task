daa24a1a4bf1498ac1a56af39b87f1d8626f1ebe
MacAlsandair <mac.alsandair@gmail.com>
2023-07-20 14:46:00 +0200

    First commit

---
+++ src/main/java/com/macalsandair/egartechnology/EgarTechnologyTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.macalsandair.egartechnology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgarTechnologyTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgarTechnologyTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macalsandair/egartechnology/EgarTechnologyTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macalsandair.egartechnology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgarTechnologyTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
