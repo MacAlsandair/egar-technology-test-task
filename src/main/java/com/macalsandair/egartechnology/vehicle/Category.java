b8ae4c50436d93694cd8f89553fbc8bd5a8606e4
MacAlsandair <mac.alsandair@gmail.com>
2023-07-20 15:19:21 +0200

    Create Category and TypeOfVehicle enums

---
+++ src/main/java/com/macalsandair/egartechnology/vehicle/Category.java
@@ -0,0 +1,11 @@
+package com.macalsandair.egartechnology.vehicle;
+
+public enum Category {
+	
+	A,
+	B,
+	C,
+	D,
+	E;
+
+}
---
+++ src/main/java/com/macalsandair/egartechnology/vehicle/TypeOfVehicle.java
@@ -0,0 +1,7 @@
+package com.macalsandair.egartechnology.vehicle;
+
+public enum TypeOfVehicle {
+	
+	
+
+}
