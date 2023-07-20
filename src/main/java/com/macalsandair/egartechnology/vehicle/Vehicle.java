ef57469550f1a17bb62e8b0973c006b13b74c698
MacAlsandair <mac.alsandair@gmail.com>
2023-07-20 15:44:55 +0200

    Add getters, setters and constructors to Vehicle, change category field to String

--- src/main/java/com/macalsandair/egartechnology/vehicle/Vehicle.java
+++ src/main/java/com/macalsandair/egartechnology/vehicle/Vehicle.java
@@ -13,9 +13,70 @@ public abstract class Vehicle {
     private Long id;
     private String brand;
     private String model;
-    private Category category;
+    private String category;
     private String stateNumber;
     private int yearOfManufacture;
     private boolean hasTrailer;
+    
+    
+	public Vehicle() {
+		super();
+	}
+	
+	public Vehicle(String brand, String model, String category, String stateNumber, int yearOfManufacture,
+			boolean hasTrailer) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.category = category;
+		this.stateNumber = stateNumber;
+		this.yearOfManufacture = yearOfManufacture;
+		this.hasTrailer = hasTrailer;
+	}    
+    
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	public String getStateNumber() {
+		return stateNumber;
+	}
+	public void setStateNumber(String stateNumber) {
+		this.stateNumber = stateNumber;
+	}
+	public int getYearOfManufacture() {
+		return yearOfManufacture;
+	}
+	public void setYearOfManufacture(int yearOfManufacture) {
+		this.yearOfManufacture = yearOfManufacture;
+	}
+	public boolean isHasTrailer() {
+		return hasTrailer;
+	}
+	public void setHasTrailer(boolean hasTrailer) {
+		this.hasTrailer = hasTrailer;
+	}
+	
 	
 }
