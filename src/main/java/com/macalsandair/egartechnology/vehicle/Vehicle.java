7c7a6f140b42ca18315ef2c84516d342695ed11c
MacAlsandair <mac.alsandair@gmail.com>
2023-07-20 15:04:44 +0200

    Create abstract class Vehicle

---
+++ src/main/java/com/macalsandair/egartechnology/vehicle/Vehicle.java
@@ -0,0 +1,21 @@
+package com.macalsandair.egartechnology.vehicle;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String brand;
+    private String model;
+    private String category;
+    private String stateNumber;
+    private int yearOfManufacture;
+    private boolean hasTrailer;
+	
+}
