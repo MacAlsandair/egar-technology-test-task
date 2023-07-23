84ff2ad67a89fa17bce4b3f0ed2c979d3d40fabd
MacAlsandair <mac.alsandair@gmail.com>
2023-07-23 16:00:16 +0200

    Add autowired for VehicleRepository

--- src/main/java/com/macalsandair/egartechnology/vehicle/VehicleServiceImpl.java
+++ src/main/java/com/macalsandair/egartechnology/vehicle/VehicleServiceImpl.java
@@ -3,6 +3,7 @@ package com.macalsandair.egartechnology.vehicle;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -10,6 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class VehicleServiceImpl implements VehicleService {
 	
+	@Autowired
 	private VehicleRepository vehicleRepository;
 
 	@Override
