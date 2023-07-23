ec310e85bbc32b42c1a7897865e849d38291d1db
MacAlsandair <mac.alsandair@gmail.com>
2023-07-23 20:42:53 +0200

    Rewrite search method

---
+++ src/main/java/com/macalsandair/egartechnology/vehicle/VehicleSearchCriteria.java
@@ -0,0 +1,58 @@
+package com.macalsandair.egartechnology.vehicle;
+
+public class VehicleSearchCriteria {
+	
+    private String brand;
+    private String model;
+    private String category;
+    private String stateNumber;
+    private String vehicleType;
+    private Integer yearOfManufacture;
+    private Boolean hasTrailer;
+    
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getStateNumber() {
+		return stateNumber;
+	}
+	public void setStateNumber(String stateNumber) {
+		this.stateNumber = stateNumber;
+	}
+	public String getVehicleType() {
+		return vehicleType;
+	}
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+	public Integer getYearOfManufacture() {
+		return yearOfManufacture;
+	}
+	public void setYearOfManufacture(Integer yearOfManufacture) {
+		this.yearOfManufacture = yearOfManufacture;
+	}
+	public Boolean getHasTrailer() {
+		return hasTrailer;
+	}
+	public void setHasTrailer(Boolean hasTrailer) {
+		this.hasTrailer = hasTrailer;
+	}
+    
+    
+
+}
--- src/main/java/com/macalsandair/egartechnology/vehicle/VehicleServiceImpl.java
+++ src/main/java/com/macalsandair/egartechnology/vehicle/VehicleServiceImpl.java
@@ -2,6 +2,7 @@ package com.macalsandair.egartechnology.vehicle;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class VehicleServiceImpl implements VehicleService {
-	
+
 	@Autowired
 	private VehicleRepository vehicleRepository;
 
@@ -19,12 +20,11 @@ public class VehicleServiceImpl implements VehicleService {
 		if (!isVehicleAlreadyExist(vehicleDTO.getStateNumber())) {
 			Vehicle createdVehicle = createVehicle(vehicleDTO);
 			return vehicleRepository.save(createdVehicle);
-		}
-		else {
+		} else {
 			throw new ResponseStatusException(HttpStatus.CONFLICT, "Этот государственный номер уже есть в системе");
 		}
 	}
-	
+
 	private Vehicle createVehicle(VehicleDTO vehicleDTO) {
 		switch (vehicleDTO.getVehicleType()) {
 		case "car":
@@ -37,7 +37,8 @@ public class VehicleServiceImpl implements VehicleService {
 			Bus newBus = new Bus(vehicleDTO);
 			return newBus;
 		default:
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Запрос некорректен. Такого типа ТС не может быть в системе");
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"Запрос некорректен. Такого типа ТС не может быть в системе");
 		}
 	}
 
@@ -47,22 +48,91 @@ public class VehicleServiceImpl implements VehicleService {
 			Vehicle updatedVehicle = createVehicle(vehicleDTO);
 			updatedVehicle.setId(id);
 			return vehicleRepository.save(updatedVehicle);
-		}
-		else {
+		} else {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Не существует тс с таким id");
 		}
 	}
 
-	@Override
-    public List<Vehicle> searchVehicles(VehicleDTO vehicleDTO) {
-        return vehicleRepository.search(vehicleDTO);
-    }
-	
-	private boolean isVehicleAlreadyExist (String stateNumber) {
+//	@Override
+//    public List<Vehicle> searchVehicles(VehicleSearchCriteria vehicleSearchCriteria) {
+//        return vehicleRepository.search(vehicleSearchCriteria);
+//    }
+
+	private boolean isVehicleAlreadyExist(String stateNumber) {
 		if (vehicleRepository.findByStateNumber(stateNumber).isPresent()) {
 			return true;
+		} else
+			return false;
+	}
+
+//	public List<Vehicle> searchVehicles(VehicleSearchCriteria searchCriteria) {
+//		  // Extract search criteria from the VehicleSearchCriteria object
+//		  String brand = searchCriteria.getBrand();
+//		  String model = searchCriteria.getModel();
+//		  String category = searchCriteria.getCategory();
+//		  String stateNumber = searchCriteria.getStateNumber();
+//		  String vehicleType = searchCriteria.getVehicleType();
+//		  Integer yearOfManufacture = searchCriteria.getYearOfManufacture();
+//		  Boolean hasTrailer = searchCriteria.getHasTrailer();
+//
+//		  // Implement search logic using Spring Data JPA
+//		  if (yearOfManufacture != null && hasTrailer != null) {
+//		    // User passed both parameters
+//		    return vehicleRepository.findByBrandAndModelAndCategoryAndStateNumberAndVehicleTypeAndYearOfManufactureAndHasTrailer(
+//		            brand, model, category, stateNumber, vehicleType, yearOfManufacture, hasTrailer);
+//		  } else if (yearOfManufacture != null) {
+//		    // User passed only yearOfManufacture parameter
+//		    return vehicleRepository.findByBrandAndModelAndCategoryAndStateNumberAndVehicleTypeAndYearOfManufacture(
+//		            brand, model, category, stateNumber, vehicleType, yearOfManufacture);
+//		  } else if (hasTrailer != null) {
+//		    // User passed only hasTrailer parameter
+//		    return vehicleRepository.findByBrandAndModelAndCategoryAndStateNumberAndVehicleTypeAndHasTrailer(
+//		            brand, model, category, stateNumber, vehicleType, hasTrailer);
+//		  } else {
+//		    // User did not pass any of the parameters
+//		    return vehicleRepository.findByBrandAndModelAndCategoryAndStateNumberAndVehicleType(
+//		            brand, model, category, stateNumber, vehicleType);
+//		  }
+//		}
+
+	public List<Vehicle> searchVehicles(VehicleSearchCriteria searchCriteria) {
+		List<Vehicle> allVehicles = vehicleRepository.findAll();
+
+		return allVehicles.stream().filter(vehicle -> isMatch(vehicle, searchCriteria)).collect(Collectors.toList());
+	}
+
+	private boolean isMatch(Vehicle vehicle, VehicleSearchCriteria searchCriteria) {
+		// Perform filtering based on each search criteria
+		if (searchCriteria.getBrand() != null 
+				&& !vehicle.getBrand().equals(searchCriteria.getBrand())) {
+			return false;
 		}
-		else return false;
+		if (searchCriteria.getModel() != null 
+				&& !vehicle.getModel().equals(searchCriteria.getModel())) {
+			return false;
+		}
+		if (searchCriteria.getCategory() != null 
+				&& !vehicle.getCategory().equals(searchCriteria.getCategory())) {
+			return false;
+		}
+		if (searchCriteria.getStateNumber() != null
+				&& !vehicle.getStateNumber().equals(searchCriteria.getStateNumber())) {
+			return false;
+		}
+		if (searchCriteria.getVehicleType() != null
+				&& !vehicle.getVehicleType().equals(searchCriteria.getVehicleType())) {
+			return false;
+		}
+		if (searchCriteria.getYearOfManufacture() != null
+				&& vehicle.getYearOfManufacture() != searchCriteria.getYearOfManufacture().intValue()) {
+			return false;
+		}
+		if (searchCriteria.getHasTrailer() != null
+				&& vehicle.isHasTrailer() != searchCriteria.getHasTrailer().booleanValue()) {
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
@@ -70,17 +140,14 @@ public class VehicleServiceImpl implements VehicleService {
 		List<Vehicle> vehicles = vehicleRepository.findAll();
 		return vehicles;
 	}
-	
+
 	public Vehicle getVehicleById(Long id) {
 		Optional<Vehicle> vehicle = vehicleRepository.findById(id);
 		if (vehicle.isPresent()) {
 			return vehicle.get();
-		}
-		else {
+		} else {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Нет транспортного средства с таким id");
 		}
 	}
-	
-	
 
 }
