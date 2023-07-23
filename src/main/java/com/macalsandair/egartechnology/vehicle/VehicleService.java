8d50c71d9d1d32587196ad735ebae15bfbe20cd3
MacAlsandair <mac.alsandair@gmail.com>
2023-07-23 05:21:23 +0200

    Add method getAllVehicles in VehicleService

--- src/main/java/com/macalsandair/egartechnology/vehicle/VehicleService.java
+++ src/main/java/com/macalsandair/egartechnology/vehicle/VehicleService.java
@@ -3,6 +3,7 @@ package com.macalsandair.egartechnology.vehicle;
 import java.util.List;
 
 public interface VehicleService {
+	List<VehicleDTO> getAllVehicles();
     VehicleDTO addVehicle(VehicleDTO vehicleDTO);
     VehicleDTO updateVehicle(VehicleDTO vehicleDTO, Long id);
     List<VehicleDTO> searchVehicles(String brand, String model);
