bd3340a21d737ded5286ed288ead07938db2a212
MacAlsandair <mac.alsandair@gmail.com>
2023-07-23 15:56:30 +0200

    Add Service annotation to VehicleServiceImpl

--- src/main/java/com/macalsandair/egartechnology/vehicle/VehicleServiceImpl.java
+++ src/main/java/com/macalsandair/egartechnology/vehicle/VehicleServiceImpl.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+@Service
 public class VehicleServiceImpl implements VehicleService {
 	
 	private VehicleRepository vehicleRepository;
