ea0f2da178378f06200f7356c68427b6b6de93ab
MacAlsandair <mac.alsandair@gmail.com>
2023-07-23 06:46:25 +0200

    Create VehicleController

--- src/main/java/com/macalsandair/egartechnology/vehicle/VehicleController.java
+++ src/main/java/com/macalsandair/egartechnology/vehicle/VehicleController.java
@@ -1,8 +1,10 @@
 package com.macalsandair.egartechnology.vehicle;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/vehicles")
@@ -10,4 +12,33 @@ public class VehicleController {
     @Autowired
     private VehicleService vehicleService;
     
+    @PostMapping
+    public ResponseEntity<Vehicle> addVehicle(@RequestBody VehicleDTO vehicleDTO) {
+        Vehicle vehicle = vehicleService.addVehicle(vehicleDTO);
+        return ResponseEntity.ok(vehicle);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Vehicle> updateVehicle(@RequestBody VehicleDTO vehicleDTO, @PathVariable Long id) {
+        Vehicle updatedVehicle = vehicleService.updateVehicle(vehicleDTO, id);
+        return ResponseEntity.ok(updatedVehicle);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Vehicle> getVehicleById(@PathVariable Long id) {
+        Vehicle vehicle = vehicleService.getVehicleById(id);
+        return ResponseEntity.ok(vehicle);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Vehicle>> getAllVehicles() {
+        List<Vehicle> vehicles = vehicleService.getAllVehicles();
+        return ResponseEntity.ok(vehicles);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<VehicleDTO>> searchVehicles(@RequestBody VehicleDTO vehicleDTO) {
+        List<VehicleDTO> vehicles = vehicleService.searchVehicles(vehicleDTO);
+        return ResponseEntity.ok(vehicles);
+    }
 }
