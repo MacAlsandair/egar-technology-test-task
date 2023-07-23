2f648ddb74c9ed2197bbe7f81438e43aea707d15
MacAlsandair <mac.alsandair@gmail.com>
2023-07-23 05:06:27 +0200

    Remove unnecessary imports in the Car class

--- src/main/java/com/macalsandair/egartechnology/vehicle/Car.java
+++ src/main/java/com/macalsandair/egartechnology/vehicle/Car.java
@@ -1,9 +1,5 @@
 package com.macalsandair.egartechnology.vehicle;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 public class Car extends Vehicle {
 
 	public Car() {
