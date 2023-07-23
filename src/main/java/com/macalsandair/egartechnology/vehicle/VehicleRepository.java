90aa5dcca2d9b7f7c4793d581ff25deb79c7929d
MacAlsandair <mac.alsandair@gmail.com>
2023-07-23 06:22:25 +0200

    Create isVehicleAlreadyExists method

--- src/main/java/com/macalsandair/egartechnology/vehicle/VehicleRepository.java
+++ src/main/java/com/macalsandair/egartechnology/vehicle/VehicleRepository.java
@@ -1,12 +1,13 @@
 package com.macalsandair.egartechnology.vehicle;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     List<Vehicle> findByBrandAndModel(String brand, String model);
-    Vehicle findByStateNumber(String stateNumber);
+    Optional<Vehicle> findByStateNumber(String stateNumber);
     //List<Vehicle> findByBrandAndModelAndCategoryAndStateNumberAndYearOfManufactureAndHasTrailer(String brand, String model);
     List<Vehicle> findByField(String brand, String model);
     
